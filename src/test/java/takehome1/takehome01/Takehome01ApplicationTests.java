9dd086a307f7706de7b9ed3bbe335ff3c2d00292
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2022-10-31 23:10:30 +0700

    first commit

---
+++ src/test/java/takehome1/takehome01/Takehome01ApplicationTests.java
@@ -0,0 +1,13 @@
+package takehome1.takehome01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Takehome01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
